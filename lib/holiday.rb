355d98f1ef6f2e7f587477ce222cfaeada2868f5
Amit Singh <amit_nyc@live.com>
2018-06-12 15:34:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  new_array=[]
+  new_array=[] #to store the values of holiday names that values matches "BBQ"
   holiday_hash.each do|season,holiday|
     holiday.each do|holiday_name,supply_array|
       supply_array.each do|supplies|
